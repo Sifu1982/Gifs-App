da47e5aa0cfdafc5e26c578cce2f4d112e1f385c
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-09-20 14:30:10 +0200

    feat: añadida variable de plantilla en el input para poder escribir y que al presionar enter se mande la información

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  // @ViewChild busca en el HTML por referencias, por clases y por variables de plantilla, que es lo que he usado en este caso
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //Non-null assertion operator -> !  Se usa para asegurarse que el objeto no es nulo y no es undefined
 
-  constructor() { }
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor);
 
-  ngOnInit(): void {
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
