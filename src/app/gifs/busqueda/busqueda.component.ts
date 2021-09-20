3e656a20cc045e4a3bdaaabca8612995fdbdde8f
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-09-20 21:00:34 +0200

    feat: app finished

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,9 +10,16 @@ export class BusquedaComponent {
   // @ViewChild busca en el HTML por referencias, por clases y por variables de plantilla, que es lo que he usado en este caso
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //Non-null assertion operator -> !  Se usa para asegurarse que el objeto no es nulo y no es undefined
 
+  constructor(private gifsService: GifsService) {}
+
   buscar() {
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor);
+
+    if (valor.trim().length === 0) {
+      return;
+    }
+
+    this.gifsService.buscarGifs(valor);
 
     this.txtBuscar.nativeElement.value = '';
   }
